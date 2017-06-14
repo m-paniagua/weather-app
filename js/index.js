c501adfa4955533e181450a04fb215bb1b6efe8b
Manuel Paniagua <manuel.paniagua84@gmail.com>
2017-06-14 12:54:30 -0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+// get lat and long
+var API_KEY = '271d38ae6cbd9ace72d6432471f794fd';
+var celcius = false;
+var weather;
+
+function displayTemp(temp, c) {
+  if(c) {
+    return Math.round((temp - 32) * (5/9)) + " C";
+  }
+  else {
+    return Math.round(temp) + " F";
+  }
+}
+
+function render(weather, celcius) {
+  var currLoc = weather.name;
+  var currWthr = weather.weather[0].description;
+  var currentTemp = displayTemp(weather.main.temp, celcius);
+  var high = displayTemp(weather.main.temp_max, celcius);
+  var low = displayTemp(weather.main.temp_min, celcius);
+  var icon = weather.weather[0].icon;
+
+  $("#header").text('Local Weather For ' + currLoc);
+  $('#current').text('Condition: ' + currWthr);
+  $('#currTemp').text('Current: ' + currentTemp);
+  $('#highLow').text('High/Low: ' + high + '/' + low);
+  // $('#high').text('Low: ' + low);
+
+  var url = 'http://openweathermap.org/img/w/' + icon + '.png';
+  $('#current').prepend('<img src="' + url + '"><br>');  
+}
+
+$(function() {
+  var zip;  
+  
+  function getWeather() {
+    // get zip code
+    $.getJSON('https://ipinfo.io/', function(data){
+      zip = data.postal;
+       // console.log(zip);
+      
+        // get current weather
+        $.getJSON('https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&units=imperial&appid=' + API_KEY, function(data) {
+        weather = data;
+        
+        render(weather, celcius);
+        
+          $('#toggle').on('click', function() {
+            celcius = !celcius;
+            render(weather, celcius);
+          });
+      })
+    })
+  }
+  
+  getWeather();
+});
